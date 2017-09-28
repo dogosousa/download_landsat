76f2e41864ad426e6382d12bf3322df13d1bc07d
dogosousa <dogosousa@gmail.com>
2017-09-27 22:04:58 -0300

    Iniciando o projeto. Realizando consulta das imagens disponíveis no repositório do USGS

---
+++ landsatteste.py
@@ -0,0 +1,25 @@
+from landsat import landsat, downloader
+import geopandas as gpd
+
+
+l = landsat.Search()
+
+s = l.search(paths_rows="215, 068",
+             lat=None,
+             lon=None,
+             address=None,
+             start_date="2015-01-01",
+             end_date="2015-12-31",
+             cloud_min=None,
+             cloud_max=16,
+             limit=1,
+             geojson=True)
+
+df = gpd.GeoDataFrame.from_features(s)
+
+lc = [x for x in df['sceneID']]
+
+d = downloader.Downloader(download_dir='~/Download/LC8',
+                          usgs_user='user_name', usgs_pass='password')
+
+d.download(lc)
